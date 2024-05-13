d74f1284cb68b5aba8e49f77055f28cb33701cdb
Gensyn <gensyn@posteo.de>
2024-05-13 15:13:34 +0200

    Refactored strings into constants and gave them a hierarchy Showing the entity icon is now optional

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from plugins.de_gensyn_HomeAssistantPlugin.const import EMPTY_STRING, HOME_ASSISTANT, SETTING_HOST, SETTING_PORT, \
+    SETTING_SSL, SETTING_TOKEN
+
 from src.backend.PluginManager.ActionHolder import ActionHolder
 from src.backend.PluginManager.PluginBase import PluginBase
 from .HomeAssistant import HomeAssistantBackend
@@ -12,22 +15,22 @@ class HomeAssistant(PluginBase):
             plugin_base=self,
             action_base=HomeAssistantAction,
             action_id="de_gensyn_HomeAssistantPlugin::HomeAssistantAction",
-            action_name="Home Assistant",
+            action_name=HOME_ASSISTANT,
         )
         self.add_action_holder(self.home_assistant_action_holder)
 
         self.register(
-            plugin_name="Home Assistant",
+            plugin_name=HOME_ASSISTANT,
             github_repo="https://github.com/gensyn/de_gensyn_HomeAssistantPlugin",
-            plugin_version="0.9.0-beta",
-            app_version="1.4.11-beta"
+            plugin_version="0.9.1-beta",
+            app_version="1.5.0-beta"
         )
 
         settings = self.get_settings()
-        host = settings.setdefault("host", "")
-        port = settings.setdefault("port", "")
-        ssl = settings.setdefault("ssl", True)
-        token = settings.setdefault("token", "")
+        host = settings.setdefault(SETTING_HOST, EMPTY_STRING)
+        port = settings.setdefault(SETTING_PORT, EMPTY_STRING)
+        ssl = settings.setdefault(SETTING_SSL, True)
+        token = settings.setdefault(SETTING_TOKEN, EMPTY_STRING)
 
         self.backend = HomeAssistantBackend()
         self.backend.set_host(host)
@@ -38,10 +41,10 @@ class HomeAssistant(PluginBase):
     def set_settings(self, settings):
         super().set_settings(settings)
 
-        host = settings.setdefault("host", "")
-        port = settings.setdefault("port", "")
-        ssl = settings.setdefault("ssl", True)
-        token = settings.setdefault("token", "")
+        host = settings.setdefault(SETTING_HOST, EMPTY_STRING)
+        port = settings.setdefault(SETTING_PORT, EMPTY_STRING)
+        ssl = settings.setdefault(SETTING_SSL, True)
+        token = settings.setdefault(SETTING_TOKEN, EMPTY_STRING)
 
         self.backend.set_host(host)
         self.backend.set_port(port)
