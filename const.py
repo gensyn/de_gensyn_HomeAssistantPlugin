340c75940acee94f816e5f5f2e199769fd0cabe5
Gensyn <gensyn@posteo.de>
2024-05-14 09:39:02 +0200

    Added option to show entity text

---
+++ const.py
@@ -0,0 +1,71 @@
+EMPTY_STRING = ""
+HOME_ASSISTANT = "Home Assistant"
+
+CONNECT_NOTIFY_ACTIVE = "notify::active"
+CONNECT_NOTIFY_TEXT = "notify::text"
+
+# BASE
+LABEL_HOST_KEY = "actions.base.host.label"
+LABEL_PORT_KEY = "actions.base.port.label"
+LABEL_SSL_KEY = "actions.base.ssl.label"
+LABEL_TOKEN_KEY = "actions.base.token.label"
+LABEL_SETTINGS_KEY = "actions.base.settings.label"
+
+SETTING_HOST = "host"
+SETTING_PORT = "port"
+SETTING_SSL = "ssl"
+SETTING_TOKEN = "token"
+
+# HOME_ASSISTANT_ACTION
+CONNECT_BIND = "bind"
+CONNECT_CHANGED = "changed"
+CONNECT_NOTIFY_SELECTED = "notify::selected"
+
+LABEL_ENTITY_DOMAIN = "actions.home_assistant.entity.domain.label"
+LABEL_ENTITY_ENTITY = "actions.home_assistant.entity.entity.label"
+
+LABEL_SERVICE_SERVICE = "actions.home_assistant.service.service.label"
+LABEL_SERVICE_CALL_ON_KEY_DOWN = "actions.home_assistant.service.call_on_key_down.label"
+LABEL_SERVICE_CALL_ON_KEY_UP = "actions.home_assistant.service.call_on_key_up.label"
+
+LABEL_ICON_SHOW_ICON = "actions.home_assistant.icon.show_icon.label"
+
+LABEL_TEXT_SHOW_TEXT = "actions.home_assistant.text.show_text.label"
+LABEL_TEXT_POSITION = "actions.home_assistant.text.position.label"
+LABEL_TEXT_SIZE = "actions.home_assistant.text.size.label"
+LABEL_TEXT_ADAPTIVE_SIZE = "actions.home_assistant.text.adaptive_size.label"
+LABEL_TEXT_SHOW_UNIT = "actions.home_assistant.text.show_unit.label"
+LABEL_TEXT_UNIT_LINE_BREAK = "actions.home_assistant.text.unit_line_break.label"
+LABEL_TEXT_ATTRIBUTE = "actions.home_assistant.text.attribute.label"
+
+LABEL_SETTINGS_ENTITY = "actions.home_assistant.settings.entity.label"
+LABEL_SETTINGS_SERVICE = "actions.home_assistant.settings.service.label"
+LABEL_SETTINGS_ICON = "actions.home_assistant.settings.icon.label"
+LABEL_SETTINGS_TEXT = "actions.home_assistant.settings.text.label"
+
+SETTING_ENTITY_DOMAIN = "entity.domain"
+SETTING_ENTITY_ENTITY = "entity.entity"
+
+SETTING_SERVICE_SERVICE = "service.service"
+SETTING_SERVICE_SERVICE_ON_KEY_DOWN = "service.service_on_key_down"
+SETTING_SERVICE_SERVICE_ON_KEY_UP = "service.service_on_key_up"
+
+SETTING_ICON_SHOW_ICON = "icon.show_icon"
+
+SETTING_TEXT_SHOW_TEXT = "text.show_text"
+SETTING_TEXT_POSITION = "text.position"
+SETTING_TEXT_ADAPTIVE_SIZE = "text.adaptive_size"
+SETTING_TEXT_SIZE = "text.size"
+SETTING_TEXT_SHOW_UNIT = "text.show_unit"
+SETTING_TEXT_UNIT_LINE_BREAK = "text.unit_line_break"
+SETTING_TEXT_ATTRIBUTE = "text.attribute"
+
+STATE = "state"
+
+ATTRIBUTES = "attributes"
+ATTRIBUTE_FRIENDLY_NAME = "friendly_name"
+ATTRIBUTE_UNIT_OF_MEASUREMENT = "unit_of_measurement"
+
+TEXT_POSITION_TOP = "top"
+TEXT_POSITION_CENTER = "center"
+TEXT_POSITION_BOTTOM = "bottom"
