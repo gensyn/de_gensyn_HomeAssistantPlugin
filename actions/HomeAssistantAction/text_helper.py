a5c53059d8f7429a5a4c39ce036c9cf8e7808c0b
Gensyn <gensyn@posteo.de>
2024-11-21 08:18:45 +0100

    Fixed value not being rounded when unit enabled

--- actions/HomeAssistantAction/text_helper.py
+++ actions/HomeAssistantAction/text_helper.py
@@ -17,6 +17,8 @@ def get_text(state: Dict, settings: Dict):
     attribute = settings.get(const.SETTING_TEXT_ATTRIBUTE)
 
     if attribute == const.STATE:
+        text = _round_value(text, settings)
+
         if settings.get(const.SETTING_TEXT_SHOW_UNIT):
             unit = state.get(const.ATTRIBUTES, {}).get(const.ATTRIBUTE_UNIT_OF_MEASUREMENT,
                                                        const.EMPTY_STRING)
@@ -30,18 +32,8 @@ def get_text(state: Dict, settings: Dict):
                 text_length = len(text)
     else:
         text = str(state.get(const.ATTRIBUTES, {}).get(attribute, const.EMPTY_STRING))
+        text = _round_value(text, settings)
         text_length = len(text)
-
-    if settings.get(const.SETTING_TEXT_ROUND, const.DEFAULT_TEXT_ROUND) and _is_float(text):
-        precision = int \
-            (settings.get(const.SETTING_TEXT_ROUND_PRECISION, const.DEFAULT_TEXT_ROUND_PRECISION))
-
-        as_number = round(float(text), precision)
-
-        if precision == 0:
-            as_number = int(as_number)
-
-        text = str(as_number)
 
     position = settings.get(const.SETTING_TEXT_POSITION)
     font_size = settings.get(const.SETTING_TEXT_SIZE)
@@ -64,7 +56,22 @@ def get_text(state: Dict, settings: Dict):
     return text, position, font_size
 
 
-def _is_float(value: str):
+def _round_value(text: str, settings: Dict) -> str:
+    if not settings.get(const.SETTING_TEXT_ROUND, const.DEFAULT_TEXT_ROUND) or not _is_float(text):
+        return text
+
+    precision = int(
+        settings.get(const.SETTING_TEXT_ROUND_PRECISION, const.DEFAULT_TEXT_ROUND_PRECISION))
+
+    as_number = round(float(text), precision)
+
+    if precision == 0:
+        as_number = int(as_number)
+
+    return str(as_number)
+
+
+def _is_float(value: str) -> bool:
     if not "." in value:
         return False
 
